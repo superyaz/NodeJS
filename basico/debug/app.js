f3a70a3f2f2b50fe325bde5dc74db5dd1ddb5496
superyaz <yacamocursos@hotmail.com>
2019-09-02 21:57:10 -0500

    Continuamos con los ejercicios de Node

---
+++ basico/debug/app.js
@@ -0,0 +1,12 @@
+let curso = "node";
+
+curso = "node.js";
+curso = "node.js tutos";
+
+console.log(curso);
+
+x = ()=>{
+    return 1 + 1; 
+}
+
+console.log(x());
---
+++ basico/debug/extra.js
@@ -0,0 +1,8 @@
+let mensaje = "Debugeando Coder! ";
+
+function saludar(){
+    console.log(mensaje);
+}
+module.exports = {
+    saludar:saludar
+}
